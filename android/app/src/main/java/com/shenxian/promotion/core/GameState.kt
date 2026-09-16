68975d1706f1bb8e6c5bd0d57099f87da9910bca
bersavesab-hue <bersavesab@gmail.com>
2026-09-16 18:53:48 +0800

    extend unified state model

--- android/app/src/main/java/com/shenxian/promotion/core/GameState.kt
+++ android/app/src/main/java/com/shenxian/promotion/core/GameState.kt
@@ -1,4 +1,4 @@
-package com.shenxian.promotion.core
+package com.shenxian.promotion
 
 class GameState {
     var day: Int = 1
@@ -6,8 +6,9 @@ class GameState {
     var incense: Int = 0
     var position: String = "无品神籍"
     var identity: String = "凡人"
+    var reputation: Int = 0
 
     fun summary(): String {
-        return "身份：$identity\n职位：$position\n日期：第${day}日\n功德：$merit\n香火：$incense"
+        return "身份：$identity\n职位：$position\n日期：第${day}日\n功德：$merit\n香火：$incense\n声望：$reputation"
     }
 }
