0a3fe1324e588b645412d33be3f9371c733c8b0d
bersavesab-hue <bersavesab@gmail.com>
2026-09-16 17:50:39 +0800

    增加核心游戏状态数据层

---
+++ android/app/src/main/java/com/shenxian/promotion/core/GameState.kt
@@ -0,0 +1,13 @@
+package com.shenxian.promotion.core
+
+class GameState {
+    var day: Int = 1
+    var merit: Int = 0
+    var incense: Int = 0
+    var position: String = "无品神籍"
+    var identity: String = "凡人"
+
+    fun summary(): String {
+        return "身份：$identity\n职位：$position\n日期：第${day}日\n功德：$merit\n香火：$incense"
+    }
+}
